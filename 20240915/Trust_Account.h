0674d6821179fe93821d908018b44e44e3d9a3f5
MTDau <50783337+MTDau@users.noreply.github.com>
2024-09-15 18:02:03 -0700

    Update Trust_Account.h

--- 20240915/Trust_Account.h
+++ 20240915/Trust_Account.h
@@ -1,8 +1,6 @@
-#include "Account.h"
-#include "Account_Util.h"
 #pragma once
-#ifndef _TRUST_ACCOUNT_H
-#define _TRUST_ACCOUNT_H
+#include "Savings_Account.h"
+
 
 class Trust_Account : public Savings_Account
 {
@@ -10,6 +8,7 @@ class Trust_Account : public Savings_Account
 
 private:
     unsigned int bonus_mark{5000};
+    unsigned int bonus{50};
     const unsigned int withdrawn_limit{3};
     int withdrawn_time{0};
     static constexpr const char *def_name = "Unnamed Savings Account";
@@ -21,6 +20,4 @@ public:
     Trust_Account(std::string name = def_name, double balance = def_balance, double int_rate = def_int_rate);
     bool deposit(double &des);
     bool withdraw(double &withdraw);
-    int get_time_withdrawn() const;
 };
-#endif
