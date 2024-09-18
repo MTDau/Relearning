a0dc523583233cbb4d83393cb7ed64484b9921a7
MTDau <50783337+MTDau@users.noreply.github.com>
2024-09-18 16:55:31 -0700

    Update Account_Util.cpp

--- 20240918/Account_Util.cpp
+++ 20240918/Account_Util.cpp
@@ -8,6 +8,12 @@ void display(const std::vector<std::shared_ptr<Account>> &accounts)
     for (const auto &acc : accounts)
         std::cout << *acc << std::endl;
 }
+void display(const std::vector<std::unique_ptr<Account>> &accounts)
+{
+    std::cout << "\n=== Accounts===========================================" << std::endl;
+    for (const auto &acc : accounts)
+        std::cout << *acc << std::endl;
+}
 
 // Deposits supplied amount to each Account object pointer in the vector
 void deposit(std::vector<std::shared_ptr<Account>> &accounts, double amount)
@@ -21,9 +27,31 @@ void deposit(std::vector<std::shared_ptr<Account>> &accounts, double amount)
             std::cout << "Failed Deposit of " << amount << " to " << *acc << std::endl;
     }
 }
+void deposit(std::vector<std::unique_ptr<Account>> &accounts, double amount)
+{
+    std::cout << "\n=== Depositing =================================" << std::endl;
+    for (auto &acc : accounts)
+    {
+        if (acc->deposit(amount))
+            std::cout << "Deposited " << amount << " to " << *acc << std::endl;
+        else
+            std::cout << "Failed Deposit of " << amount << " to " << *acc << std::endl;
+    }
+}
 
 // Withdraw amount from each Account object pointer in the vector
 void withdraw(std::vector<std::shared_ptr<Account>> &accounts, double amount)
+{
+    std::cout << "\n=== Withdrawing ==============================" << std::endl;
+    for (auto &acc : accounts)
+    {
+        if (acc->withdraw(amount))
+            std::cout << "Withdrew " << amount << " from " << *acc << std::endl;
+        else
+            std::cout << "Failed Withdrawal of " << amount << " from " << *acc << std::endl;
+    }
+}
+void withdraw(std::vector<std::unique_ptr<Account>> &accounts, double amount)
 {
     std::cout << "\n=== Withdrawing ==============================" << std::endl;
     for (auto &acc : accounts)
